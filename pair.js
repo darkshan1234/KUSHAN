4ffbd72d7a2dda5afbaa0699f08af445c7cfe4f4
darkshan1234 <149415238+darkshan1234@users.noreply.github.com>
2024-03-08 19:33:46 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -33,7 +33,7 @@ router.get('/', async (req, res) => {
                 },
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: [ "Ubuntu", "Chrome", "20.0.04" ],
+                browser: Browsers.windows('Firefox'),
              });
              if(!session.authState.creds.registered) {
                 await delay(1500);
